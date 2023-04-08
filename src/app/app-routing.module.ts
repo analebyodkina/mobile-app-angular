1abf47e3fa21c25da5a3e0c0499da8590c83458a
analebyodkina <dp030992cad@gmail.com>
2023-04-08 18:04:54 +0300

    added basic structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
