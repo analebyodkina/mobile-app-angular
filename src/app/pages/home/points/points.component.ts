ed6aeb1f53fb570c39619eb02c6b7ca2cc9a8f5b
analebyodkina <dp030992cad@gmail.com>
2023-04-14 13:44:51 +0300

    added acordion to home page component

--- src/app/pages/home/points/points.component.ts
+++ src/app/pages/home/points/points.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  } from '@angular/animations';
+
 @Component({
   selector: 'app-points',
   templateUrl: './points.component.html',
-  styleUrls: ['./points.component.scss']
+  styleUrls: ['./points.component.scss'],
+  animations: [
+    trigger('openClose', [
+      transition('void => *', [
+        style({ opacity: 0, transform: 'translateY(-10px)' }),
+        animate('.3s', style({ 
+          opacity: 1,
+          transform: 'translateY(0)'
+        })),
+      ]),
+      transition('* => void', [
+        animate('.3s', style({ 
+          opacity: 0,
+          transform: 'translateY(-10px)'
+        })),
+      ]),
+    ])
+  ]
 })
 export class PointsComponent {
+  public toggle: boolean = false;
+  public unfold: boolean = false;
 
 }
